34996e7e7858b19838b46dfb9f8ac8a7b6aac2bc
farahalashi <farahemad20001@gmail.com>
2021-07-29 23:00:24 +0200

    function to get all data from API relates#9

---
+++ cafe.js
@@ -0,0 +1,12 @@
+
+const api = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail";
+async function getDatta() {
+  try {
+    const response = await fetch(api);
+    const data = await response.json();
+    createList(data);
+  } catch (error) {
+    console.log("error", error.message);
+  }
+}
+getDatta();
