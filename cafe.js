2e0ce2e6492ec19c669eae09e921b43f89f033f3
farahalashi <farahemad20001@gmail.com>
2021-07-30 11:47:37 +0200

    create function to create elements and put data from API in it relates#10

--- cafe.js
+++ cafe.js
@@ -10,3 +10,34 @@ async function getDatta() {
   }
 }
 getDatta();
+const container = document.getElementById("container");
+
+function createList(data) {
+  let arrayOfDrinks = data.drinks;
+
+  for (let i = 0; i < 25; i++) {
+    const divs = document.createElement("div");
+    divs.setAttribute("class", "drink");
+    container.appendChild(divs);
+    const figureCaption = document.createElement("figure");
+    figureCaption.setAttribute("class", "fig");
+    divs.appendChild(figureCaption);
+    const imge = document.createElement("img");
+    imge.setAttribute("class", "imgs");
+    figureCaption.appendChild(imge);
+    imge.setAttribute("src", `${arrayOfDrinks[i].strDrinkThumb}`);
+
+    const caption1 = document.createElement("figcaption");
+    caption1.setAttribute("class", "caption");
+    figureCaption.appendChild(caption1);
+
+    caption1.innerText = arrayOfDrinks[i].strDrink;
+
+    const caption2 = document.createElement("figcaption");
+    caption2.setAttribute("class", "caption  captions");
+    figureCaption.appendChild(caption2);
+    let price = arrayOfDrinks[i].idDrink;
+    let slice = price.slice(0, 2);
+    caption2.innerText = ` ${slice} $`;
+  }
+}
